58d146c8b44d603c29ea39470b41e30a72f46d0b
Anirudh Pal <pal5@purdue.edu>
2020-04-22 17:01:22 -0400

    Desc

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+// Import a Standard Library Reference
 var http = require('http');
 
-//create a server object:
+// Create a Server
 http.createServer(function (req, res) {
 	res.write('Hello World!'); //write a response to the client
 	res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(8080); // Host on 8080 Port
