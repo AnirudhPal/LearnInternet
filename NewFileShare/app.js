97e06dc765b329d664be962156dc2e89d96b6591
Morgan <mburgett@purdue.edu>
2020-05-08 17:57:24 -0500

    Server Changes

---
+++ NewFileShare/app.js
@@ -0,0 +1,73 @@
+//Import libraries
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
+//Whenever browser requests additional files, go to /public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Send HTML when requesting access to website
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+//Revceice files and send progress data for mills
+app.post('/mill', function (req, res) {
+    //Save File
+    saveFile('/mill', req, res);
+});
+
+//Revceice files and send progress data for lathes
+app.post('/lathe', function (req, res) {
+    //Save File
+    saveFile('/lathe', req, res);
+});
+
+//Revceice files and send progress data for gantry
+app.post('/gantry', function (req, res) {
+    //Save File
+    saveFile('/gantry', req, res);
+});
+
+//Revceice files and send progress data for water jet
+app.post('/waterjet', function (req, res) {
+    //Save File
+    saveFile('/waterjet', req, res);
+});
+
+//Function for saving the file
+function saveFile(loc, req, res) {
+
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, loc);
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        res.end('success');
+    });
+    // parse the incoming request containing the form data
+    form.parse(req);
+}
+
+
+var server = app.listen(3000, function () {
+    console.log('Server listening on port 3000');
+});
---
+++ NewFileShare/public/javascripts/upload.js
@@ -0,0 +1,107 @@
+//Variable to track device
+var device = '';
+
+//Function for Mill Upload
+function millUpload() {
+    //Set device
+    device = '/mill';
+
+    //Clear and Run
+    clearRun();
+}
+
+//Function for Lathe Upload
+function latheUpload() {
+    //Set device
+    device = '/lathe';
+
+    //Clear and Run
+    clearRun();
+}
+
+//Function for Gantry Upload
+function gantryUpload() {
+    //Set device
+    device = '/gantry';
+
+    //Clear and Run
+    clearRun();
+}
+
+//Function for Waterjet Upload
+function waterjetUpload() {
+    //Set device
+    device = '/waterjet';
+
+    //Clear and Run
+    clearRun();
+}
+
+//Function for Clearing Progress Bar
+function clearRun() {
+    //Trigger input
+    $('#upload-input').click();
+
+    //Clear Progress Bar
+    $('.progress-bar').text('0%');
+    $('.progress-bar').width('0%');
+
+    //Print statement
+    console.log('Sending File to ' + device + '\n');
+}
+
+//Allows user to send data to server
+$('#upload-input').on('change', function () {
+
+    //Get files that are associated with the input
+    var files = $(this).get(0).files;
+
+    //Only proceed if there is more than one file selected
+    if (files.length > 0) {
+
+        // create a FormData object which will be sent as the data payload in the AJAX request
+        var formData = new FormData();
+
+        // loop through all the selected files and add them to the formData object
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            // add the files to formData object for the data payload
+            formData.append('uploads[]', file, file.name);
+        }
+
+        //Creating an AJAX object
+        $.ajax({
+            url: device,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                console.log('Upload Successful :)\n' + data);
+            },
+            xhr: function () {
+                // create an XMLHttpRequest
+                var xhr = new XMLHttpRequest();
+
+                // listen to the 'progress' event
+                xhr.upload.addEventListener('progress', function (evt) {
+                    if (evt.lengthComputable) {
+                        // calculate the percentage of upload completed
+                        var percentComplete = evt.loaded / evt.total;
+                        percentComplete = parseInt(percentComplete * 100);
+
+                        // update the Bootstrap progress bar with the new percentage
+                        $('.progress-bar').text(percentComplete + '%');
+                        $('.progress-bar').width(percentComplete + '%');
+
+                        // once the upload reaches 100%, set the progress bar text to done
+                        if (percentComplete === 100) {
+                            $('.progress-bar').html('Go to the machine if you dare...');
+                        }
+                    }
+                }, false);
+                return xhr;
+            }
+        });
+    }
+});
